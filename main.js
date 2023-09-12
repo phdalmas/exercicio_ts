1d8de66bc83eebad03c2a95d525c80be48499647
Pedro Dalmas <phdalforno@gmail.com>
2023-09-12 16:11:45 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplication(number1, number2) {
+    return number1 * number2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+function multiplication(number1: number, number2: number): number {
+    return number1 * number2;
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`
+}
